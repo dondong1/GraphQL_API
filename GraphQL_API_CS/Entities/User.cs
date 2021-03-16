56458dbee0d6b60bf2cbb8a42430b98657866a2f
Don <don_nguyen2001@yahoo.com>
2021-03-16 11:44:35 -0700

    abc

---
+++ GraphQL_API_CS/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GraphQL_API.Entities
+{
+    public partial class User
+    {
+        public User()
+        {
+            Customers = new HashSet<Customer>();
+            Employees = new HashSet<Employee>();
+        }
+
+        public long Id { get; set; }
+        public string email { get; set; }
+        public string encrypted_password { get; set; }
+        public string reset_password_token { get; set; }
+        public DateTime? reset_password_sent_at { get; set; }
+        public DateTime? remember_created_at { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public boolen  superadmin_role, default: false
+        public boolean employee_role, default: false
+        public boolean user_role, default: true
+
+        public virtual ICollection<Customer> Customers { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
--- GraphQL_API_CS/Startup.cs
+++ GraphQL_API_CS/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using GraphQL_API_CS.Entities;
 
 namespace GraphQL_API_CS
 {
@@ -16,6 +18,9 @@ namespace GraphQL_API_CS
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList"));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
